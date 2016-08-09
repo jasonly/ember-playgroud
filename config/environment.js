f74d7ea3e7ae35761e93d9b64d4ae8ee2b59c1d3
Jason Ly <ja_ly@apple.com>
2016-08-09 13:47:37 -0700

    [Task] Switched to pod structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-playground',
+    podModulePrefix: 'ember-playground/pods',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
