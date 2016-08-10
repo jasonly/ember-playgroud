18a18267e271d6e8bce32f05bd57dab0861dceb5
Jason Ly <ja_ly@apple.com>
2016-08-10 16:21:37 -0700

    [Task] Implemented infinte scrolling but need to fix the meta data

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,4 +1,33 @@
-import { JSONAPISerializer } from 'ember-cli-mirage';
+// import { JSONAPISerializer } from 'ember-cli-mirage';
 
-export default JSONAPISerializer.extend({
+// export default JSONAPISerializer.extend({
+// });
+
+
+import { Serializer } from 'ember-cli-mirage';
+
+export default Serializer.extend({
+	serialize(res) {
+		let data = [];
+
+
+		res.models.forEach(function(item, i) {
+			let taco = {
+				attributes: item.attrs,
+				id: i,
+				type: 'infinitescrolls'
+			};
+
+			data.push(taco);
+		});
+
+		let payload = {
+			data: data,
+			meta: {
+				total_pages: 4
+			}
+		};
+
+		return payload;
+	}
 });
