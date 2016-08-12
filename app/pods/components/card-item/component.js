d41bc57e760d3c04d0685d4a94c7b4490d051b4a
Jason Ly <ja_ly@apple.com>
2016-08-12 13:34:32 -0700

    [Task] notes

--- app/pods/components/card-item/component.js
+++ app/pods/components/card-item/component.js
@@ -2,4 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNames: ['card-item']
+
+	// Eventually this component should be clickable
+	// and probably use the ID from the element to do
+	// a route transition to another page.
 });
