1ded7e06ee7f132258eea88d189fbf9d4c28dd33
Jason Ly <ja_ly@apple.com>
2016-08-12 11:24:09 -0700

    [Task] Added crappy styles

--- mirage/factories/infinitescroll.js
+++ mirage/factories/infinitescroll.js
@@ -3,5 +3,5 @@ import { Factory, faker } from 'ember-cli-mirage';
 export default Factory.extend({
   name(i) { return `Person ${i}`; },
   age: 28,
-  avatar() { return faker.internet.avatar(); }
+  avatar() { return faker.image.avatar(); }
 });
