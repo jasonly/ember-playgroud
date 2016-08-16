a8404dc97e5f022435b5552dcd1694e50b899311
Shawn Zhu <shawn.zhu@level-studios.com>
2016-08-16 14:55:47 -0700

    add liquid-fire 	modified:   app/pods/components/card-item/component.js -- use <li/> to list the card item 	modified:   app/pods/components/card-item/style.scss  -- style list items 	modified:   app/pods/components/card-item/template.hbs -- use <li/> 	new file:   app/pods/infinitescroll/style.scss -- route specific style 	modified:   app/pods/infinitescroll/template.hbs -- update route UI 	modified:   app/styles/pods.scss -- add infinitescroll route style 	modified:   package.json -- load liquid-fire

--- app/pods/components/card-item/component.js
+++ app/pods/components/card-item/component.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['card-item']
+  tagName: 'li',
+	classNames: ['card-item'],
 
 	// Eventually this component should be clickable
 	// and probably use the ID from the element to do
